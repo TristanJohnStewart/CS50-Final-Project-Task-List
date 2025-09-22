3bfa546aa9781a673435e9d5b68a840cf15252a7
The-Comet-Gaming <tjstew880@gmail.com>
2025-09-22 18:45:23 +0100

    Created TaskController for handling all Task manipulation related enquires. Including creating generic GET methods for create, edit, delete. Organised View folders into correct structure. Added some elements to Create.cshtml.

---
+++ CS50TaskList/CS50TaskList/Controllers/TaskController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CS50TaskList.Controllers
+{
+    public class TaskController : Controller
+    {
+        // GET: Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // GET: Delete
+        public ActionResult Delete()
+        {
+            return View();
+        }
+
+        // GET: Edit
+        public ActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
