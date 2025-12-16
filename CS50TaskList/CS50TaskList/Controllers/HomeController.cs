46481ccd56dc3e4f4c8d931805b95598d1bd5804
Tristan Stewart <tjstew880@gmail.com>
2025-12-16 13:29:36 +0000

    Changed Error, Home to intialise a new ErrorViewModel on a seperate line and have the action return the newly intialised model.

--- CS50TaskList/CS50TaskList/Controllers/HomeController.cs
+++ CS50TaskList/CS50TaskList/Controllers/HomeController.cs
@@ -34,7 +34,8 @@ namespace CS50TaskList.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            var model = new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier };
+            return View(model);
         }
     }
 }
