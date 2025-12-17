798869efc82f887dc7df2d2b1f271175fb7af86b
Tristan Stewart <tjstew880@gmail.com>
2025-12-17 17:35:02 +0000

    Add logs for the start, end/s, variable assignment and repository interactions to methods in SubTask Service.

--- CS50TaskList/CS50TaskList/Services/SubTaskService.cs
+++ CS50TaskList/CS50TaskList/Services/SubTaskService.cs
@@ -1,6 +1,7 @@
 ﻿using CS50TaskList.Data.Entities;
 using CS50TaskList.Models;
 using CS50TaskList.Repositories;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Threading.Tasks;
 
@@ -9,70 +10,126 @@ namespace CS50TaskList.Services
     public class SubTaskService : ISubTaskService
     {
         private readonly IRepository<SubTask> _repository;
-        public SubTaskService(IRepository<SubTask> repository)
+        private readonly ILogger<SubTaskService> _logger;
+
+        public SubTaskService(IRepository<SubTask> repository, ILogger<SubTaskService> logger)
         {
             _repository = repository;
+            _logger = logger;
         }
 
         public async System.Threading.Tasks.Task CompleteTaskAsync(int id)
         {
+            _logger.LogInformation("Beginning SubTaskService CompleteTaskAsync Method for Subtask #{id}.", id);
             try
             {
+                _logger.LogInformation("Retrieving Subtask Entity #{id} from DB.", id);
                 var entity = await _repository.GetByIdAsync(id);
+
+                _logger.LogInformation("Updating Subtask Entity #{id} IsComplete property to {!entity.IsCompleted} from {entity.IsCompleted}", id, !entity.IsCompleted, entity.IsCompleted);
                 entity.IsCompleted = !entity.IsCompleted;
+
+                _logger.LogInformation("Updating Subtask Entity #{id} in DB.", id);
                 _repository.Update(entity);
+
+                _logger.LogInformation("Saving changes to DB.");
                 await _repository.SaveChangesAsync();
+
+                _logger.LogInformation("Successfully ending SubTaskService CompleteTaskAsync Method for Subtask #{id}.", id);
             }
-            catch (Exception ex) { throw; }
+            catch (Exception ex) 
+            {
+                _logger.LogError("Error occured in SubTaskService CompleteTaskAsync Method. Error: {ex}", ex);
+                throw; 
+            }
         }
 
         public async System.Threading.Tasks.Task CreateSubTaskAsync(SubTaskModel model)
         {
+            _logger.LogInformation("Beginning SubTaskService CreateSubTaskAsync Method to create Subtask for Task #{model.ParentId}.", model.ParentId);
             try
             {
+                _logger.LogInformation("Intialising new Subtask Entity.");
                 var entity = new SubTask 
                 { 
                     Title = model.Title,
                     Position = model.Position,
                     TaskId = model.ParentId
-                };            
+                };
 
+                _logger.LogInformation("Creating new Subtask entry in the DB.");
                 await _repository.Create(entity);
+
+                _logger.LogInformation("Saving changes to DB.");
                 await _repository.SaveChangesAsync();
+
+                _logger.LogInformation("Successfully ending SubTaskService CreateSubTaskAsync Method.");
             }
-            catch (Exception ex) { throw; }
+            catch (Exception ex) 
+            {
+                _logger.LogError("Error occured in SubTaskService CreateSubTaskAsync Method. Error: {ex}", ex);
+                throw; 
+            }
         }
 
         public async System.Threading.Tasks.Task DeleteSubTaskAsync(SubTaskModel model)
         {
+            _logger.LogInformation("Beginning SubTaskService DeleteSubTaskAsync Method to delete Subtask #{model.Id} from DB.", model.Id);
             try
             {
+                _logger.LogInformation("Retrieving Subtask Entity #{model.Id} from DB.", model.Id);
                 var entity = await _repository.GetByIdAsync(model.Id);
+
+                _logger.LogInformation("Removing Subtask #{model.Id} from DB.", model.Id);
                 _repository.Remove(entity);
+
+                _logger.LogInformation("Saving changes to DB.");
                 await _repository.SaveChangesAsync();
+
+                _logger.LogInformation("Successfully ending SubTaskService DeleteSubTaskAsync Method.");
             }
-            catch (Exception ex) { throw; }
+            catch (Exception ex) 
+            {
+                _logger.LogError("Error occured in SubTaskService DeleteSubTaskAsync Method. Error: {ex}", ex);
+                throw; 
+            }
         }
 
         public async System.Threading.Tasks.Task EditSubTaskAsync(SubTaskModel model)
         {
+            _logger.LogInformation("Beginning SubTaskService EditSubTaskAsync Method to edit Subtask #{model.Id} in DB.", model.Id);
             try
             {
+                _logger.LogInformation("Retrieving Subtask Entity #{model.Id} from DB.", model.Id);
                 var entity = await _repository.GetByIdAsync(model.Id);
+
+                _logger.LogInformation("Asigning new Subtask Entity Title property.");
                 entity.Title = model.Title;
 
+                _logger.LogInformation("Updating Subtask #{model.Id} in DB.", model.Id);
                 _repository.Update(entity);
+
+                _logger.LogInformation("Saving changes to DB.");
                 await _repository.SaveChangesAsync();
+
+                _logger.LogInformation("Successfully ending SubTaskService EditSubTaskAsync Method.");
             }
-            catch (Exception ex) { throw; }
+            catch (Exception ex) 
+            {
+                _logger.LogError("Error occured in SubTaskService EditSubTaskAsync Method. Error: {ex}", ex);
+                throw; 
+            }
         }
 
         public async Task<SubTaskModel> PrepareForDeleteAsync(int id)
         {
+            _logger.LogInformation("Beginning SubTaskService PrepareForDeleteAsync Method to prepare Subtask #{id} for the Delete View.", id);
             try
             {
+                _logger.LogInformation("Retrieving Subtask Entity #{id} from DB.", id);
                 var entity = await _repository.GetByIdAsync(id);
 
+                _logger.LogInformation("Intialising new Subtask Entity for Subtask #{id}.", id);
                 var model = new SubTaskModel
                 {
                     Id = id,
@@ -80,17 +137,25 @@ namespace CS50TaskList.Services
                     ParentId = entity.TaskId
                 };
 
+                _logger.LogInformation("Sucessfully returning model of Subtask #{id}.", id);
                 return model;
             }
-            catch (Exception ex) { throw; }
+            catch (Exception ex) 
+            {
+                _logger.LogError("Error occured in SubTaskService PrepareForDeleteAsync Method. Error: {ex}", ex);
+                throw; 
+            }
         }
 
         public async Task<SubTaskModel> PrepareForEditAsync(int id)
         {
+            _logger.LogInformation("Beginning SubTaskService PrepareForEditAsync Method to prepare Subtask #{id} for the Delete View.", id);
             try
             {
+                _logger.LogInformation("Retrieving Subtask Entity #{id} from DB.", id);
                 var entity = await _repository.GetByIdAsync(id);
 
+                _logger.LogInformation("Intialising new Subtask Entity for Subtask #{id}.", id);
                 var model = new SubTaskModel
                 {
                     Id = id,
@@ -98,9 +163,14 @@ namespace CS50TaskList.Services
                     ParentId = entity.TaskId
                 };
 
+                _logger.LogInformation("Sucessfully returning model of Subtask #{id}.", id);
                 return model;
             }
-            catch (Exception ex) { throw; }
+            catch (Exception ex) 
+            {
+                _logger.LogError("Error occured in SubTaskService PrepareForEditAsync Method. Error: {ex}", ex);
+                throw; 
+            }
         }
     }
 }
